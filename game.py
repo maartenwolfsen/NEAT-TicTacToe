48e0b92c62546aa5d6b205180877eabae597be50
Maarten Wolfsen <maartenwolfsen@gmail.com>
2022-07-07 15:21:21 +0200

    Create class for defined functions

--- game.py
+++ game.py
@@ -1,100 +1,114 @@
 import random
 
-USER_INPUT_STRING = "Your turn (use 0-9 to place at that position)"
+class TicTacToe:
+    USER_INPUT_STRING = "Your turn (use 0-9 to place at that position)"
 
-def in_array(array, search):
-    for item in search:
-        if item not in array:
-            return False
+    # Todo: Variable grid size
+    def __init__(self, grid_size):
+        self.turn = True
+        self.field = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.p1_moves = []
+        self.p2_moves = []
+        self.grid_size = grid_size
 
-    return True
+    # Check if combination is reached
+    def in_field(self, player_field, search):
+        for item in search:
+            if item not in player_field:
+                return False
 
-def user_input(field, input):
-    if field[input] != 0:
-        # fitness -1
-        input = user_input(int(input(USER_INPUT_STRING)))
-
-    return input
-
-def check_win(field):
-    p1_moves = []
-    p2_moves = []
-    i = 0
-
-    for value in field:
-        if value == 1: p1_moves.append(i)
-        if value == 2: p2_moves.append(i)
-
-        i += 1
-
-    if check_player(p1_moves): return 1
-    if check_player(p2_moves): return 2
-
-    return 0
-
-def check_player(player):
-    if (in_array(player, [0, 1, 2]) or
-        in_array(player, [3, 4, 5]) or
-        in_array(player, [6, 7, 8]) or
-        in_array(player, [0, 3, 6]) or
-        in_array(player, [1, 4, 7]) or
-        in_array(player, [2, 5, 8]) or
-        in_array(player, [0, 4, 8]) or
-        in_array(player, [2, 4, 6])):
         return True
 
-    return False
+    # User input
+    def user_input(self, input):
+        if self.field[input] != 0:
+            # fitness -1
+            input = self.user_input(int(input(self.USER_INPUT_STRING)))
 
-def opponent_move(field):
-    open = []
-    i = 0
+        return input
 
-    for spot in field:
-        if spot == 0:
-            open.append(i)
+    # Check if win conditions are met
+    def check_win(self):
+        i = 0
 
-        i += 1
+        for value in self.field:
+            if value == 1: self.p1_moves.append(i)
+            if value == 2: self.p2_moves.append(i)
 
-    return open[random.randint(0, len(open) - 1)]
+            i += 1
 
-def print_field(field):
-    print(str(field[0]) + str(field[1]) + str(field[2]))
-    print(str(field[3]) + str(field[4]) + str(field[5]))
-    print(str(field[6]) + str(field[7]) + str(field[8]))
+        if self.check_player(self.p1_moves): return 1
+        if self.check_player(self.p2_moves): return 2
 
-def end_turn(field):
-    print_field(field)
-    win = check_win(field)
+        return 0
 
-    if win != 0:
-        print("Player " + str(win) + " won!")
+    # Check if player has met win conditions
+    # Todo: Dynamic checking
+    def check_player(self, player):
+        if (self.in_field(player, [0, 1, 2]) or
+            self.in_field(player, [3, 4, 5]) or
+            self.in_field(player, [6, 7, 8]) or
+            self.in_field(player, [0, 3, 6]) or
+            self.in_field(player, [1, 4, 7]) or
+            self.in_field(player, [2, 5, 8]) or
+            self.in_field(player, [0, 4, 8]) or
+            self.in_field(player, [2, 4, 6])):
+            return True
 
         return False
 
-    return True
+    # Opponent move (random move)
+    def opponent_move(self):
+        open = []
+        i = 0
+
+        for spot in self.field:
+            if spot == 0:
+                open.append(i)
+
+            i += 1
+
+        return open[random.randint(0, len(open) - 1)]
+
+    # Print TicTacToe field
+    def print_field(self):
+        print("[" + str(self.field[0]) + str(self.field[1]) + str(self.field[2]) + "]")
+        print("[" + str(self.field[3]) + str(self.field[4]) + str(self.field[5]) + "]")
+        print("[" + str(self.field[6]) + str(self.field[7]) + str(self.field[8]) + "]")
+
+    # End turn
+    def end_turn(self):
+        self.turn = not self.turn
+        win = self.check_win()
+
+        if win != 0:
+            print("Player " + str(win) + " won!")
+
+            return False
+
+        return True
 
 def main():
-    field = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    turn = True
+    game = TicTacToe(3)
+    game.print_field()
     run = True
-    print_field(field)
 
     while run:
-        if not turn:
-            field[opponent_move(field)] = 2
-            turn = not turn
-            run = end_turn(field)
+        # Opponent turn
+        if not game.turn:
+            game.field[game.opponent_move()] = 2
+            run = game.end_turn()
 
             continue
 
-        u_input = user_input(
-            field,
+        # Player turn
+        u_input = game.user_input(
             int(input(
-                "[" + str(1 if turn else 2) + "] " + USER_INPUT_STRING)
+                "[" + str(1 if game.turn else 2) + "] " + game.USER_INPUT_STRING)
             )
         )
-        field[u_input] = 1
-        turn = not turn
-        run = end_turn(field)
+        game.field[u_input] = 1
+        run = game.end_turn()
+        game.print_field()
 
 main()
