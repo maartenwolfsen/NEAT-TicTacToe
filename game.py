f31d9422cc3704d4aec3ee9ddafc65191b523c63
Maarten Wolfsen <maartenwolfsen@gmail.com>
2022-07-08 11:55:40 +0200

    Add NEAT Networks

--- game.py
+++ game.py
@@ -1,4 +1,10 @@
+from os import path
 import random
+import neat
+import pickle
+
+ROOT_DIR = path.dirname(__file__)
+ETC_DIR = path.join(ROOT_DIR, 'etc')
 
 class TicTacToe:
     USER_INPUT_STRING = "Your turn (use 0-9 to place at that position)"
@@ -22,8 +28,7 @@ class TicTacToe:
     # User input
     def user_input(self, input):
         if self.field[input] != 0:
-            # fitness -1
-            input = self.user_input(int(input(self.USER_INPUT_STRING)))
+            return -1
 
         return input
 
@@ -88,27 +93,127 @@ class TicTacToe:
 
         return True
 
-def main():
-    game = TicTacToe(3)
-    game.print_field()
-    run = True
+    def output_to_move(self, output):
+        move = 0
 
-    while run:
-        # Opponent turn
-        if not game.turn:
-            game.field[game.opponent_move()] = 2
-            run = game.end_turn()
+        if output > -1.75 and output <= -1.25:
+            move = 1
+        elif output > -1.25 and output <= -0.75:
+            move = 2
+        elif output > -0.75 and output <= -0.25:
+            move = 3
+        elif output > -0.25 and output <= 0.25:
+            move = 4
+        elif output > 0.25 and output <= 0.75:
+            move = 5
+        elif output > 0.75 and output <= 1.25:
+            move = 6
+        elif output > 1.25 and output <= 1.75:
+            move = 7
+        else:
+            move = 8
 
-            continue
+        return move
 
-        # Player turn
-        u_input = game.user_input(
-            int(input(
-                "[" + str(1 if game.turn else 2) + "] " + game.USER_INPUT_STRING)
-            )
-        )
-        game.field[u_input] = 1
-        run = game.end_turn()
-        game.print_field()
+    def check_if_moves_left(self):
+        for move in self.field:
+            if move == 0:
+                return True
 
-main()
+        return False
+
+def main(genomes, config):
+    networks = []
+    ge = []
+    games = []
+
+    # Setup Neural Networks
+    for _, g in genomes:
+        network = neat.nn.FeedForwardNetwork.create(g, config)
+        networks.append(network)
+        games.append(TicTacToe(3))
+        g.fitness = 0
+        ge.append(g)
+
+    for index, game in enumerate(games):
+        run = True
+
+        while run:
+            if not game.check_if_moves_left():
+                ge[index].fitness -= 0.5
+                print("No moves left")
+                games.pop(index)
+                networks.pop(index)
+                ge.pop(index)
+                run = False
+
+                continue
+
+            # Opponent turn
+            if not game.turn:
+                game.field[game.opponent_move()] = 2
+                result = game.end_turn()
+
+                if result == False:
+                    game.print_field()
+                    ge[index].fitness -= 1
+                    games.pop(index)
+                    networks.pop(index)
+                    ge.pop(index)
+
+                run = result
+
+                continue
+
+            # Player turn
+            input_data = game.field
+            output = networks[index].activate(input_data)
+            move = game.output_to_move(output[0])
+            print("Player move: " + str(move))
+            u_input = game.user_input(move)
+
+            # Invalid move
+            if u_input == -1:
+                ge[index].fitness -= 1
+                games.pop(index)
+                networks.pop(index)
+                ge.pop(index)
+                run = False
+                print("Player 1 lost because of invalid move")
+
+                continue
+
+            game.field[u_input] = 1
+            result = game.end_turn()
+
+            # Game won
+            if result == False:
+                game.print_field()
+                game = TicTacToe(3)
+                ge[index].fitness += 1
+                games[index] = game
+                result = True
+
+# Run AI
+def run(config_path):
+    config = neat.config.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path
+    )
+
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    p.add_reporter(neat.StatisticsReporter())
+
+    winner = p.run(main)
+
+
+    with open('winner', 'wb') as f:
+        pickle.dump(winner, f)
+
+# Set Config
+if __name__ == "__main__":
+    run(path.join(ETC_DIR, "config"))
